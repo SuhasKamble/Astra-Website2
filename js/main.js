b3ab864ae6c216d6551e71c58d508a64bde72ade
Suhas <kamblesuhas12@gmail.com>
2021-01-15 14:36:23 +0530

    Added pages

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 const burger = document.querySelector('.burger')
 const navLinks = document.querySelector('.navLinks')
 const bars = document.querySelector('.fa-bars')
+const links = document.querySelectorAll('.navLinks li a')
+
+
 
 burger.addEventListener('click',()=>{
     navLinks.classList.toggle('show') 
-    bars.classList.toggle('fa-times')       
+    bars.classList.toggle('fa-times') 
+    
+    console.log(links)
 })
+
+links.forEach(element => {
+    element.addEventListener('click',()=>{
+        navLinks.classList.remove('show')
+        bars.classList.remove('fa-times')
+    })
+});
